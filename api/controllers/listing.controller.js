e586c5cd0514092e64207e1e6cd2a6aee1498467
sezai besiktepe <sezaibsktp@gmail.com>
2023-10-20 16:09:18 +0300

    complete user delete functionality

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,9 +1,25 @@
 import { listingModel } from "../models/listing.js";
+import { errorHandler } from "../utils/error.js";
 
 export const createListing = async (req, res, next) => {
   try {
     const listing = await listingModel.create(req.body);
     return res.status(201).json(listing);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteListing = async (req, res, next) => {
+  const listing = await listingModel.findById(req.params.id);
+  if (!listing) return next(errorHandler(404, "Listing not found !"));
+  if (req.user.id !== listing.userRef) {
+    return next(errorHandler(401, "You can delete only your own listings !!"));
+  }
+
+  try {
+    await listingModel.findByIdAndDelete(req.params.id);
+    res.status(200).json("");
   } catch (error) {
     next(error);
   }
--- api/routes/listRouter.js
+++ api/routes/listRouter.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { createListing } from "../controllers/listing.controller.js";
+import {
+  createListing,
+  deleteListing,
+} from "../controllers/listing.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const listRouter = express.Router();
 
 listRouter.post("/create", verifyToken, createListing);
+listRouter.delete("/delete/:id", verifyToken, deleteListing);
 
 export { listRouter };
